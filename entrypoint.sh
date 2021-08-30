d91072ce57736699acdef17e29526955ca393a6c
i4amtrazy <88360975+i4amtrazy@users.noreply.github.com>
2021-08-30 16:36:15 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,5 @@ wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 unzip ngrok-stable-linux-amd64.zip -d /usr/local/bin
 chmod +x /usr/local/bin/ngrok
 /usr/local/bin/ngrok authtoken 1hNbxLEqFed9ugmNXxkmzCD2lfC_3o76kjxQF2gGyqLh87HWh
+echo "web_addr: $PORT" >> .ngrok2/ngrok.yml
 /usr/local/bin/ngrok tcp 3389 > /dev/null 2>&1
