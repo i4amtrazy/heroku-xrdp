cc6f2c0de5ccbaffb32d02ee79bb0928632232a9
i4amtrazy <88360975+i4amtrazy@users.noreply.github.com>
2021-08-30 15:02:19 +0530

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+apt install build-essential xorg xfce4 xfce4-goodies firefox xrdp git wget
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+unzip ngrok-stable-linux-amd64.zip -d /usr/local/bin
+xrdp > /dev/null 2>&1
+ngrok authtoken 1hNbxLEqFed9ugmNXxkmzCD2lfC_3o76kjxQF2gGyqLh87HWh
+ngrok tcp 3389
