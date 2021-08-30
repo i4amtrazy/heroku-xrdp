99dbd1835b385d8b6cca5ff5c8f6a934989d949a
i4amtrazy <88360975+i4amtrazy@users.noreply.github.com>
2021-08-30 15:12:38 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-apt install build-essential xorg xfce4 xfce4-goodies firefox xrdp git wget
+apt-get update
+apt install build-essential xorg xfce4 xfce4-goodies firefox xrdp git wget -y
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 unzip ngrok-stable-linux-amd64.zip -d /usr/local/bin
 xrdp > /dev/null 2>&1
