7e5e17e956a125a034581b0c88bf9caeae07cb1e
i4amtrazy <88360975+i4amtrazy@users.noreply.github.com>
2021-08-30 16:12:09 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,5 @@ useradd -M iamtrazy && echo "iamtrazy:i@mtrazy" | chpasswd
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 unzip ngrok-stable-linux-amd64.zip -d /usr/local/bin
 chmod +x /usr/local/bin/ngrok
-xrdp
 /usr/local/bin/ngrok authtoken 1hNbxLEqFed9ugmNXxkmzCD2lfC_3o76kjxQF2gGyqLh87HWh
-/usr/local/bin/ngrok tcp 3389 &
+/usr/local/bin/ngrok tcp 3389 > /dev/null 2>&1
